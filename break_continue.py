3ac73e61a33c58a0fd83821616574c9da99957bf
Abhay Rastogi <rabhay@rabhay-a01.vmware.com>
2020-01-09 05:20:02 +0530

    abhay

---
+++ break_continue.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""
+i = 2
+
+
+while(i < 5):
+    i = i+1
+    print(i)
+"""
+i=0
+while(True):
+    if i < 5:
+        i = i + 1
+        continue
+    print (i)
+    i = i + 1
+    if i == 44:
+        break
+    
