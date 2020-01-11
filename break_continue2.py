ca8b1d060833c4efa71e437ecf92eb596a473639
Abhay Rastogi <rabhay@rabhay-a01.vmware.com>
2020-01-12 04:59:18 +0530

    abhay

--- break_continue2.py
+++ break_continue2.py
@@ -7,6 +7,8 @@ while(True):
     if number < 100:
         print (number)
         number = number + 1
-    if number >= 100:
+    elif number >= 100:
         print("congratulation ur no is greater than  or equal to 100")
         break
+    else:
+        print"some other error ask abhay to check"
---
+++ guess_no_program.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+Guess = 0
+value = 33
+
+while(Guess < 9):
+    Guess = Guess + 1
+    number = int(raw_input("enter ur no :--- "))
+    if value > number :
+        print("enter value is lesser than result")
+    elif value == number :
+        print ("you enter the correct number")
+        print "you took %r chance to give correct answer" %(Guess)
+        break
+    else :
+        print("value is greater ")
