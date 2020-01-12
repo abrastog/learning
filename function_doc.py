ec95a460a55eeac60a60b6e1bbb3acf738fc823b
Abhay Rastogi <rabhay@rabhay-a01.vmware.com>
2020-01-13 02:21:35 +0530

    abhay

--- function_doc.py
+++ function_doc.py
@@ -6,4 +6,23 @@ def abhay():
     return appuchu #always use return function in ur program
 
 
-print (abhay())    
+print (abhay())   
+
+
+
+
+
+""" here we are talking about doc string in python """
+
+a = int(raw_input("enter the value here:-"))
+b = int(raw_input("enter the value here:-"))
+
+def abhi(a,b):
+    """ here we are using this function to take 2 input and do sum of them """
+    sum = (a+b)
+    return sum
+
+
+print(abhi.__doc__)
+v = abhi(a,b)
+print "value of abhi function is :-", v
